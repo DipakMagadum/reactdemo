61bda4873e18d7111d9ca2a3a9d80d9d60f26d90
DipakMagadum09 <dipakmagadum09@gmail.com>
2026-03-23 15:06:16 +0000

    update App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import {Routes,Route } from 'react-router-dom'
 import './App.css'
 import { Login } from "./Login.jsx";
-
+import {Home} from "./Home.jsx";
 function App() {
 
   return (
     <>
      <Routes>
-      <Route path="/" element={<Login/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
      </Routes>
     </>
   )
