0c1b3226d1f99b5f44f7d47b4b849da81ffe0c12
DipakMagadum09 <dipakmagadum09@gmail.com>
2026-03-23 16:49:41 +0000

    added navbar in home page

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,8 +1,30 @@
+import {Link} from "react-router-dom";
 export function Home(){
 
     return(
         <>
-        <h1> This is home page</h1>
+        <div >
+           <nav className="flex bg-sky-600 h-20 items-center justify-end p-5 ">
+             <ul>
+
+                <div className=" flex  gap-7 text-lg text-white ">
+
+                    <li className="border p-2 " ><input type="text" /></li>
+                <li className="hover:bg-sky-500 p-1 rounded">
+                    <Link to="/login">Login</Link>
+                </li>
+
+                <li  className="hover:bg-sky-500 p-1 rounded">
+                    <Link to="/">Home</Link>
+                </li>
+
+                <li  className="hover:bg-sky-500 p-1 rounded">
+                     <Link to="/Logout">Logout</Link>
+                </li>
+                </div>
+            </ul>
+           </nav>
+        </div>
         </>
     )
 }
