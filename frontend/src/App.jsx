b2f85ae44a98ee9a5610e24f24d02e55f5b9d791
Maruti96 <ymaruti96@gmail.com>
2026-03-24 02:09:43 +0000

    login and router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes,Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 import { Login } from "./Login.jsx";
 import {Home} from "./Home.jsx";
@@ -6,10 +6,15 @@ function App() {
 
   return (
     <>
+    <Router>
+      <nav>
+        <Link to="/">Login</Link>
+      </nav>
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
      </Routes>
+    </Router>
     </>
   )
 }
--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,42 @@
+export { useState } from "react";
 export function Login (){
+
+    const [Username,setname]=useState("")
+    const [Password,setpassword]=useState("")
+    const [error,seterror]=useState({Username:"",Password:""})
+
+    function call(e){
+
+       e.preventDefault()
+       let newerror = {Username:"",Password:""}
+
+       if(Username=""){
+        newerror.name="please enter Username"
+       }
+       if(Password=""){
+        newerror.password="please enter password"
+       }
+
+       seterror(newerror)
+    }
+
     return(
         <>
+        <form>
+
         <h1>Login Page</h1>
+
+        <label>Username</label>
+        <input type=text placeholder="please enter registered Username">
+
+        <label>Password</label>
+        <input type="text" placeholder="please enter password">
+
+        <button>LOGIN<button>
+
+        <button>Register</button>   
+
+        </form>
         </>
     )
 }
