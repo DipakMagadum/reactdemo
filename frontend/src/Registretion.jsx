e033d5e6997f18044f3eb6ed0a663f6ddfc9a528
Dattatray patil <dattapatil862002@gmail.com>
2026-03-24 14:56:40 +0000

    update registration page

--- frontend/src/Registretion.jsx
+++ frontend/src/Registretion.jsx
@@ -3,8 +3,8 @@ export function Registretion(){
   const[name,setname]=useState("")
   const[lname,setlname]=useState("")
   const[email,setemail]=useState("")
-  const[gender,setgender]=useState(false)
-  const[city,setcity]=useState(false)
+  const[gender,setgender]=useState("")
+  const[city,setcity]=useState("")
   const[error,seterror]=useState({name:"",lname:"",email:"",gender:"",city:""})
    
   function Regcall(e){
@@ -24,49 +24,50 @@ export function Registretion(){
         validate=false
     } else if (!email.includes("@")){
         Msg.email="invalid email"
-    }if(gender==false){
+    }if(!gender){
         Msg.gender="Select gender"
         validate=false
-    }if(city==false){
+    }if(!city){
         Msg.city="Select city"
         validate=false
     } seterror(Msg)
     if(validate){
       alert("Registration succesfully")
     };
-    seterror(Msg)
   } 
     return(
       <>
       <div className="bg-amber-100">
       <form 
        className="flex justify-center item-center h-screen" onSubmit={Regcall} noValidate>
-        <div className="border-3  justify-center item-center h-125 w-80 m-20 bg-gray-200 shadow-xl/50">
-            <div className=" justify-center item-center m-10">
-            <h1 className="text-center text-xl mt-10" style={{fontSize:25}}><b>Registration Form</b></h1>
+        <div className="border-3  justify-center item-center h-130 w-80 m-5 bg-gray-200 shadow-xl/50">
+            <div className=" justify-center item-center m-2 ml-10">
+            <h1 className="text-center text-xl " style={{fontSize:25}}><b>Registration Form</b></h1>
             <label >Name</label><br></br>
-             <input className="border p-1 w-60 bg-white  " type="text" placeholder="Enter your name"onChange={(e)=>setname(e.target.value)} /><br></br>
+             <input className="border p-1 w-60 bg-white  " type="text" placeholder="Enter your name"onChange={(e)=>setname(e.target.value)} />
              <p className="text-red-500">{error.name}</p>
              <label>Last Name:</label><br></br>
-            <input className="border p-1 w-60 bg-white" type="text"placeholder="Enter Last Name"onChange={(e)=>setlname(e.target.value)}/><br></br>
+            <input className="border p-1 w-60 bg-white" type="text"placeholder="Enter Last Name"onChange={(e)=>setlname(e.target.value)}/>
             <p className="text-red-500">{error.lname}</p>
             <label>Email:</label><br></br>
-            <input className="border p-1 w-60 bg-white"  type="email" placeholder="Enter Email" onChange={(e)=>setemail(e.target.value)}/><br></br>
+            <input className="border p-1 w-60 bg-white"  type="email" placeholder="Enter Email" onChange={(e)=>setemail(e.target.value)}/>
             <p className="text-red-500">{error.email}</p>
             <label> Gender:</label><br></br>
-           <input className="ml-2" type="radio" name="gender"/>Male
-                    <input className="ml-2" type="radio" name="gender"/>Female
-                    <input className="ml-2" type="radio" name="gender"/>Other<br></br>
+           <input className="ml-2" type="radio" name="gender" onChange={(e)=>setgender(e.target.value)}/>Male
+                    <input className="ml-2" type="radio" name="gender" onChange={(e)=>setgender(e.target.value)}/>Female
+                    <input className="ml-2" type="radio" name="gender" onChange={(e)=>setgender(e.target.value)}/>Other
+                    <p className="text-red-500">{error.gender}</p>
                     <label>City:</label>
-              <select className="bg-white ml-10 mt-5 h-8">
+              <select className="bg-white ml-10 mt-5 h-8" onChange={(e)=>setcity(e.target.value)}>
                 <option>Select City</option>
                     <option>Pune</option>
                     <option>Mumbai</option>
                     <option>Dheli</option>
                     <option>Kolhapur</option>
                     <option>Belgam</option>
-                </select><br></br>
-                <button className="cursor-pointer border-1 shadow-xl/10 ml-18 mt-5 h-10 w-25 rounded-2xl bg-blue-100">Submit</button>
+                </select>
+                <p className="text-red-500">{error.city}</p>
+                <button className="cursor-pointer border-1 shadow-xl/10 ml-18 mt-2 h-10 w-25 rounded-2xl bg-blue-100 ">Submit</button>
                 </div>
         </div>
      </form>
